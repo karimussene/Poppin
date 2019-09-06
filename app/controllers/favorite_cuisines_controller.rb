c503dfa12b8a326ee9c2839cab95dcf06f000faf
T909 <ijoline@gmail.com>
2019-09-06 10:43:07 +0100

    Adding card and functionality to results page Need adding card and styling

--- app/controllers/favorite_cuisines_controller.rb
+++ app/controllers/favorite_cuisines_controller.rb
@@ -5,4 +5,25 @@ class FavoriteCuisinesController < ApplicationController
     user_choices.each {|cuisine_id| FavoriteCuisine.create(user: current_user, cuisine_id: cuisine_id) }
     redirect_to results_path
   end
+  def add
+    FavoriteCuisine.create(user: current_user, cuisine_id: params[:cuisine_id])
+    redirect_to results_path
+  end
+  def destroy
+    FavoriteCuisine.find(params[:id]).destroy # id in the url in that case. Otherwise coming from a views and could be from params
+    redirect_to results_path
+  end
+  def compare
+    fc = FavoriteCuisine.find(params[:favorite_cuisine_id])
+    fc.compare = true
+    fc.save
+    redirect_to results_path
+  end
+  def uncompare
+    fc = FavoriteCuisine.find(params[:favorite_cuisine_id])
+    fc.compare = false
+    fc.save
+    redirect_to results_path
+  end
+
 end
